ce601419b9b47564c818454c4a7f374c432f9595
pankaj <pankajpardeshi997@gmail.com>
2021-08-21 06:30:35 -0700

    second

---
+++ [adpkjp.java
@@ -0,0 +1,24 @@
+		for (int i = 0; i < a.length; i++) {
+			sh.add(a[i]);
+		}
+		 		System.out.println(sh);
+		System.out.println(sh.first());
+		System.out.println(sh.last());
+		int z=(int) sh.first();
+		int y=(int) sh.last();
+		System.out.println("Addition"+(z+y));
+		
+		System.out.println(sh.headSet(4));
+		System.out.println(sh.tailSet(4));
+		System.out.println(sh.contains(2));
+		System.out.println(sh.subSet(2, 5));
+		
+		
+}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
